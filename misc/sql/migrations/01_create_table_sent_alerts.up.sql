1a2e978cb408fac64c4b47be43740126a2225d62
rsantosgomes <rsantosgomes@ext.worten.pt>
2022-04-17 15:57:19 +0100

    created sent_alerts schema

---
+++ misc/sql/migrations/01_create_table_sent_alerts.up.sql
@@ -0,0 +1,11 @@
+create table sent_alerts if not exists {
+    club_id varchar(255) not null,
+    club_name varchar(255) not null,
+    court_id varchar(255) not null,
+    court_name varchar(255) not null,
+    slot_date date not null,
+    start_time date not null,
+    end_time date not null,
+    created_at timestamp not null default NOW(),
+    updated_at timestamp not null default NOW()
+}
